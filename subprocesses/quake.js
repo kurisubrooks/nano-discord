e09ea2290885f0508a3d541a735c92a0d29716e7
Kurisu Brooks <kurisubrooks@gmail.com>
2016-04-02 16:06:03 +1100

    Post to @kurisubrooks#general only

--- subprocesses/quake.js
+++ subprocesses/quake.js
@@ -5,6 +5,8 @@ const crimson = require("crimson");
 var latestID = {};
 var latestQuake = "";
 
+// kurisubrooks#general
+var channel_id = '132368736119291904';
 
 exports.main = (discord, config, botdir) => {
     const core = require(path.join(botdir, "core.js"));
@@ -14,15 +16,15 @@ exports.main = (discord, config, botdir) => {
     function post_to_general(text) {
         _.each(discord.servers, (server, serverID) => {
             _.each(server.channels, (channel) => {
-                if(channel.name.toLowerCase() === "general") discord.sendMessage({to: channel.id, message: text});
+                if(channel.name.toLowerCase() === "general") discord.sendMessage({ to: channel_id, message: text });
             });
         });
     }
 
-    /*shake.on("connect", () => {
+    shake.on("connect", () => {
         crimson.success("Connected to Shake.");
         post_to_general("Connected to Shake.");
-    });*/
+    });
 
     shake.on("data", data => run(data));
 
@@ -44,27 +46,40 @@ exports.main = (discord, config, botdir) => {
         else if (Number(data.revision) === 1) update = "Epicenter";
         else update = "#" + (Number(data.revision) - 1);
 
-        var message = `**Earthquake Early Warning** 【${update}】\n＞ ${data.epicenter_en}\n＞ Magnitude: ${data.magnitude}, Seismic: ${data.seismic_en}, Depth: ${data.depth}`;
-        if (Number(data.situation) === 1) message += "\nhttps://maps.googleapis.com/maps/api/staticmap?center=" + data.latitude + "," + data.longitude + "&zoom=6&size=400x300&format=png&markers=" + data.latitude + "," + data.longitude + "&maptype=roadmap&style=feature:landscape.natural.terrain|hue:0x00ff09|visibility:off&style=feature:transit.line|visibility:off&style=feature:road.highway|visibility:simplified&style=feature:poi|visibility:off&style=feature:administrative.country|visibility:off&style=feature:road|visibility:off";
+        var message = `**【！】Earthquake Early Warning【 ${update} 】**\n　ー　${data.epicenter_en}\n　ー　Magnitude: ${data.magnitude}, Seismic: ${data.seismic_en}, Depth: ${data.depth}`;
+        if (Number(data.situation) === 1) message += "\nhttps://maps.googleapis.com/maps/api/staticmap?center=" + data.latitude + "," + data.longitude + "&zoom=6&size=350x250&format=png&markers=" + data.latitude + "," + data.longitude + "&maptype=roadmap&style=feature:landscape.natural.terrain|hue:0x00ff09|visibility:off&style=feature:transit.line|visibility:off&style=feature:road.highway|visibility:simplified&style=feature:poi|visibility:off&style=feature:administrative.country|visibility:off&style=feature:road|visibility:off";
 
         if (latestQuake !== data.earthquake_id) {
             latestQuake = data.earthquake_id;
-            _.each(discord.servers, (server, serverID) => {
-                _.each(server.channels, (channel) => {
-                    if (channel.name.toLowerCase() === "general")
-                        discord.sendMessage({
-                            to: channel.id,
-                            message: message
-                        }, (err, res) => { if(!err) latestID[channel.id] = res.id; });
-                });
+
+            discord.sendMessage({
+                to: channel_id,
+                message: message
+            }, (err, res) => {
+                if (!err) latestID[channel_id] = res.id;
             });
+
+            /*_.each(discord.servers, (server, serverID) => {
+                _.each(server.channels, (channel) => {
+                    if (channel.name.toLowerCase() === "general") {
+                        discord.sendMessage({
+                            to: channel_id,
+                            message: message
+                        }, (err, res) => {
+                            if (!err) latestID[channel_id] = res.id;
+                        });
+                    }
+                });
+            });*/
         } else {
             _.each(latestID, (messageID, channel) => 
                 discord.editMessage({
-                    channel: channel,
+                    channel: channel_id,
                     messageID: messageID,
                     message: message
-                }, (err, res) => { if (!err) latestID[channel] = res.id; })
+                }, (err, res) => {
+                    if (!err) latestID[channel_id] = res.id;
+                })
             );
         }
     }
