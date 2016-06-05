ab94cf75f607327b21a4bbbaf80a42225566e140
kurisubrooks <kurisubrooks@gmail.com>
2016-06-05 13:10:17 +1000

    Fixed Message Updating 🙃

--- subprocesses/quake.js
+++ subprocesses/quake.js
@@ -1,47 +1,58 @@
 const path = require("path");
+const _ = require("lodash");
 const Crimson = require("crimson");
 const crimson = new Crimson({});
 
-last_ts = "";
-last_quake = "";
+var last_id = {};
+var last_quake = "";
 
 exports.main = (bot, config, dir) => {
     const core = require(path.join(dir, "core.js"));
     const keychain = require(path.join(dir, "keychain.js"));
     const socket = require("socket.io-client")(keychain.shake);
 
-    var general = "132368736119291904"; // #general
-    var debug   = "164807725966950400"; // #code
+    //function test(){eew({id:20160605063657,drill:!1,situation:0,revision:1,details:{alarm:!1,announced:"2016/06/05 06:37:16",occurred:"2016/06/05 06:36:36",magnitude:3.6,epicenter:{id:797,en:"Offshore Western Satsuma Peninsula",ja:"\u85a9\u6469\u534a\u5cf6\u897f\u65b9\u6c96"},seismic:{en:"1",ja:"1"},geography:{lat:31.1,"long":129.3,depth:10,offshore:!0}}}),setTimeout(function(){eew({id:20160605063657,drill:!1,situation:0,revision:2,details:{alarm:!1,announced:"2016/06/05 06:37:27",occurred:"2016/06/05 06:36:36",magnitude:3.6,epicenter:{id:797,en:"Offshore Western Satsuma Peninsula",ja:"\u85a9\u6469\u534a\u5cf6\u897f\u65b9\u6c96"},seismic:{en:"1",ja:"1"},geography:{lat:31.1,"long":129.3,depth:10,offshore:!0}}})},2500),setTimeout(function(){eew({id:20160605063657,drill:!1,situation:0,revision:3,details:{alarm:!1,announced:"2016/06/05 06:37:47",occurred:"2016/06/05 06:36:36",magnitude:3.6,epicenter:{id:797,en:"Offshore Western Satsuma Peninsula",ja:"\u85a9\u6469\u534a\u5cf6\u897f\u65b9\u6c96"},seismic:{en:"1",ja:"1"},geography:{lat:31.1,"long":129.3,depth:10,offshore:!0}}})},3e3),setTimeout(function(){eew({id:20160605063657,drill:!1,situation:1,revision:4,details:{alarm:!1,announced:"2016/06/05 06:37:54",occurred:"2016/06/05 06:36:36",magnitude:3.6,epicenter:{id:797,en:"Offshore Western Satsuma Peninsula",ja:"\u85a9\u6469\u534a\u5cf6\u897f\u65b9\u6c96"},seismic:{en:"1",ja:"1"},geography:{lat:31.1,"long":129.3,depth:10,offshore:!0}}})},4e3)}
+
+    var channels = [
+        "132368736119291904", // kurisubrooks-#general
+        "188512903513505792"  // kaori-#notices
+    ];
+
+    var debug = [
+        //"164807725966950400", // kurisubrooks-#code
+        "188512903513505792"    // kaori-#notices
+    ];
 
     function posdebug(text, type) {
         setTimeout(function() {
-            if (type === "e") {
-                bot.sendMessage({
-                    to: debug,
-                    message: core.error("quake", text)
-                });
-            } else {
-                bot.sendMessage({
-                    to: debug,
-                    message: text
-                });
-            }
+            _.each(debug, (channel) => {
+                if (type === "e") {
+                    bot.sendMessage({
+                        to: channel,
+                        message: core.error("quake", text)
+                    });
+                } else {
+                    bot.sendMessage({
+                        to: channel,
+                        message: text
+                    });
+                }
+            });
         }, 1250);
     }
 
     socket.on("connect", () => socket.emit("open", { version: 2 }));
     socket.on("message", (data) => posdebug("API Message:\n" + JSON.stringify(data)), "e");
-
     socket.on("auth", (data) => {
         if (data.ok) {
             crimson.success("Shake > Connected");
             posdebug("【:ok_hand:】**Connected to Shake**");
+            //setTimeout(test, 2000);
         } else {
             posdebug("API Connection Refused:\n" + data.message, "e");
             crimson.fatal("Shake Connection Refused: " + data.message);
         }
     });
-
     socket.on("quake.eew", (data) => eew(data));
     socket.on("reconnect", () => crimson.warn("Shake > Reconnecting"));
     socket.on("disconnect", () => {
@@ -55,25 +66,19 @@ exports.main = (bot, config, dir) => {
         var msg_template;
             data = (typeof data === "object") ? data : JSON.parse(data);
 
-        // Title Template
         if (data.alarm) {
-            // やばい
             title_template = "【:bell:】**Emergency Earthquake Warning**";
             msg_template = `【:point_right:】**${data.details.epicenter.en}**\n【:point_right:】**Magnitude:** ${data.details.magnitude}, **Max. Seismic:** ${data.details.seismic.en}, **Depth:** ${data.details.geography.depth}km\n【:point_right:】@everyone`;
         }  else if (data.situation === 2) {
-            // キャンセル
             title_template = "【:no_bell:】**Earthquake Warning Cancelled**";
             msg_template = `【:point_right:】This warning has been cancelled.`;
         } else {
-            // ノーマル
             title_template = "【:loudspeaker:】**Earthquake Information**";
             msg_template = `【:point_right:】**${data.details.epicenter.en}**\n【:point_right:】**Magnitude:** ${data.details.magnitude}, **Max. Seismic:** ${data.details.seismic.en}, **Depth:** ${data.details.geography.depth}km`;
         }
 
-        // Map on Final
-        if (data.situation === 1) msg_template += `\n\nhttps://maps.googleapis.com/maps/api/staticmap?center=${data.details.geography.lat},${data.details.geography.long}&zoom=6&size=400x300&format=png&markers=${data.details.geography.lat},${data.details.geography.long}&maptype=roadmap&style=feature:landscape.natural.terrain|hue:0x00ff09|visibility:off&style=feature:transit.line|visibility:off&style=feature:road.highway|visibility:simplified&style=feature:poi|visibility:off&style=feature:administrative.country|visibility:off&style=feature:road|visibility:off`;
+        if (data.situation === 1) msg_template += `\n\nhttps://maps.googleapis.com/maps/api/staticmap?center=${data.details.geography.lat},${data.details.geography.long}&zoom=6&size=300x240&markers=${data.details.geography.lat},${data.details.geography.long}&style=feature:road%7Cvisibility:off`;
 
-        // Updates
         if (data.situation === 1) {
             update = "Final";
         } else if (data.situation === 2) {
@@ -82,31 +87,31 @@ exports.main = (bot, config, dir) => {
             update = "#" + data.revision;
         }
 
-        if (last_quake != data.id) {
+        if (last_quake !== data.id) {
             last_quake = data.id;
 
-            bot.sendMessage({
-                to: general,
-                message: `${title_template} 〈${update}〉\n${msg_template}`
-            }, (err, res) => {
-                console.log(err);
-                console.log(res);
-                if (err) posdebug(err, "e");
-                if (!err) last_ts = res.id;
-            });
-        } else {
-            setTimeout(function() {
-                bot.editMessage({
-                    to: general,
-                    messageID: last_ts,
+            _.each(channels, (channel) => {
+                bot.sendMessage({
+                    to: channel,
                     message: `${title_template} 〈${update}〉\n${msg_template}`
                 }, (err, res) => {
-                    console.log(err);
-                    console.log(res);
-                    if (err) posdebug(err, "e");
-                    if (!err) last_ts = res.id;
+                    if (err) posdebug(JSON.stringify(err, null, 4), "e");
+                    if (!err) last_id[channel] = res.id;
+                    //console.log(last_id);
                 });
-            }, 100);
+            });
+        } else {
+             _.each(last_id, (messageID, channel) => {
+                bot.editMessage({
+                    channel: channel,
+                    messageID: messageID,
+                    message: `${title_template} 〈${update}〉\n${msg_template}`
+                }, (err, res) => {
+                    if (err) posdebug(JSON.stringify(err, null, 4), "e");
+                    if (!err) last_id[channel] = res.id;
+                    //console.log(last_id);
+                });
+            });
         }
     }
 };
